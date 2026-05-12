f14ec34bfe3b95b191dbf2425e12ab1c88f467e3
Bishwo <115875934+GoldenGeneration@users.noreply.github.com>
2026-05-12 16:10:03 +0545

    Terms for reservation

--- src/pages/Reservation/index.tsx
+++ src/pages/Reservation/index.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import ReservationForm from "../../components/Reservation/ReservationForm";
+import TermsPopup from "../../components/Legal/TermsAndConditions/TermsPopup";
+
+const TERMS_KEY = "aeirc_terms_accepted";
 
 export default function ReservationPage() {
+  const [termsAccepted, setTermsAccepted] = useState(
+    () => localStorage.getItem(TERMS_KEY) === "true"
+  );
+
+  const handleAccept = () => {
+    localStorage.setItem(TERMS_KEY, "true");
+    setTermsAccepted(true);
+  };
+
   return (
     <div id="reservation-page">
-      <ReservationForm />
+      <TermsPopup show={!termsAccepted} onClose={handleAccept} />
+      {termsAccepted && <ReservationForm />}
     </div>
   );
 }
